57a65e983374a9e4c027a4290a3bdc994ef7dce9
Anushka Mokashi <anushkamokashi@gmail.com>
2024-01-15 11:20:47 +0530

    rec2

--- Recursion/print_Nto1_rec.cpp
+++ Recursion/print_Nto1_rec.cpp
@@ -1,1 +1,17 @@
 //print N to 1 with recursion
+#include<iostream>
+using namespace std;
+
+void fun(int i, int n){
+    if(i<1) return;
+    cout<<i<<endl;
+    fun(i-1,n);
+}
+int main(){
+    int n;
+    cout<<"Enter n : ";
+    cin>>n;
+    cout<<"Output is : "<<endl;
+    fun(n,n); 
+    return 0;
+}
