f18cdc643791619f1dd92da7a10736de6109bb72
Anushka Mokashi <anushkamokashi@gmail.com>
2024-02-04 23:32:58 +0530

    combination sum 2

---
+++ Recursion/comibation_sum2.cpp
@@ -0,0 +1,33 @@
+// print sbsequences whose sum is k
+#include<iostream.h>
+using namespace std;
+
+void printS(int i, vector<int> &ds, int arr[] ,int s, int sum,int n){
+    if(i==n)
+    {
+        if(s == sum){
+            //cout<<"s==sum";
+            for(auto it:ds) cout<<it<<" ";
+        }
+        cout<<endl;
+        return;
+    }
+    ds.push_back(arr[i]);
+    s+=arr[i];
+    printS(i+1,ds,arr,s,sum,n);
+    s-=arr[i];
+    ds.pop_back();
+    printS(i+1,ds,arr,s,sum,n);
+
+}
+
+int main(int argc, char const *argv[])
+{
+    int n=3;
+    vector<int> ds;
+    int sum =2;
+    int arr[n]= {1,2,1};
+    //cout<<"ok";
+    printS(0,ds,arr,0,sum,n);
+    return 0;
+}
