584242ceedb461b00074f71dd31657c9cbe054ac
Anushka Mokashi <anushkamokashi@gmail.com>
2024-02-06 23:43:31 +0530

    Combination sum 2

--- Recursion/comibation_sum2.cpp
+++ Recursion/comibation_sum2.cpp
@@ -1,33 +1,30 @@
-// print sbsequences whose sum is k
-#include<iostream.h>
+#include<iostream>
+#include<vector>
 using namespace std;
+class Solution {
 
-void printS(int i, vector<int> &ds, int arr[] ,int s, int sum,int n){
-    if(i==n)
-    {
-        if(s == sum){
-            //cout<<"s==sum";
-            for(auto it:ds) cout<<it<<" ";
+public:
+    void findCombination(int ind,int target, vector<int> &arr,vector<vector<int>>&ans,vector<int>&ds){
+        if(target==0){
+            ans.push_back(ds);
+            return;
         }
-        cout<<endl;
-        return;
+        for(int i=ind;i<arr.size();i++){
+            if(i>ind && arr[i]==arr[i-1]) continue; //if [1,1,1,2,2] as we can't pick 1st as 0th is also 1
+            //but if its at the start then pick hence i>ind
+            if(arr[i]>target) break;
+            ds.push_back(arr[i]);
+            findCombination(i+1,target-arr[i],arr,ans,ds);
+            ds.pop_back();
+        }
+        
     }
-    ds.push_back(arr[i]);
-    s+=arr[i];
-    printS(i+1,ds,arr,s,sum,n);
-    s-=arr[i];
-    ds.pop_back();
-    printS(i+1,ds,arr,s,sum,n);
-
-}
-
-int main(int argc, char const *argv[])
-{
-    int n=3;
-    vector<int> ds;
-    int sum =2;
-    int arr[n]= {1,2,1};
-    //cout<<"ok";
-    printS(0,ds,arr,0,sum,n);
-    return 0;
-}
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end()); //as we need in lexicographical order
+        vector<vector<int>>ans; //store the combination
+        vector<int> ds;  //use in recursion to generate the combination
+        findCombination(0,target,candidates,ans,ds);
+        return ans;
+    }
+};
